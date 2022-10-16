f988c24942d083819e6c3d3ff64e7c26650f40f3
Vitalii Vdovenko <vdovenko.vtl@gmail.com>
2022-10-16 19:37:11 +0300

    Regex input filter fix

--- FuzzyAlzheimerUI/MainWindow.xaml.cs
+++ FuzzyAlzheimerUI/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace FuzzyAlzheimerUI
             Theme.Apply(Theme.GetAppTheme() is ThemeType.Dark ? ThemeType.Light : ThemeType.Dark);
         }
 
-        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static readonly Regex _regex = new Regex("[^0-9,-]+"); //regex that matches disallowed text
         private void PreviewTextInputEvent(object sender, System.Windows.Input.TextCompositionEventArgs e)
         {
             e.Handled = _regex.IsMatch(e.Text);
