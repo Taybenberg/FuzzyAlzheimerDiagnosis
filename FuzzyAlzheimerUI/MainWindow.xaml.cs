028f3bb5f1460bdb3f674c9977df7c59c82a8e61
Vitalii Vdovenko <vdovenko.vtl@gmail.com>
2022-10-16 17:18:31 +0300

    Added UI layer

---
+++ FuzzyAlzheimerUI/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Wpf.Ui.Controls;
+
+namespace FuzzyAlzheimerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UiWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
